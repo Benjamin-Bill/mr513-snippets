1d3db77a9d138107aa5b041fc72db97c1183430f
vass0047 <lucas.vasseur@etudiant.univ-reims.fr>
2023-10-12 22:28:39 +0200

    5. Gestion des langages de programmation

--- src/snippets/snippets.routes.ts
+++ src/snippets/snippets.routes.ts
@@ -5,6 +5,6 @@ import {query} from "express-validator";
 import {languageValidator} from "../languages/languages.middlewares";
 const router = express.Router();
 
-router.get('/', query('lang').notEmpty().isInt().custom(async value => {await languageValidator(value)}),expressAsyncHandler(SnippetsController.list));
+router.get('/', query('lang').optional().notEmpty().isInt().custom(async value => {await languageValidator(value)}),expressAsyncHandler(SnippetsController.list));
 
 export default router;
