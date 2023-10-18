62fe9637319a6cdb9db9d971caa836f6118ab8db
vass0047 <lucas.vasseur@etudiant.univ-reims.fr>
2023-10-18 23:00:20 +0200

    Contrôle d'accès

--- src/snippets/snippets.controller.ts
+++ src/snippets/snippets.controller.ts
@@ -11,11 +11,53 @@ export class SnippetsController {
                 where: {languageId: parseInt(<string>req.query.lang)}
             }).then((snippets) => {
                 return snippets
-            }) : await prisma.snippet.findMany({include: {Language: true, User: {select: {id: true,name: true}}}}).then((snippets) => {
+            }) : await prisma.snippet.findMany({
+                include: {
+                    Language: true,
+                    User: {select: {id: true, name: true}}
+                }
+            }).then((snippets) => {
                 return snippets
             });
             const user = req.session.user;
             res.render('snippets/snippets_list', {snippets, title: 'Liste des snippets', section: 'Snippets', user});
+        } else {
+            throw new Error(result.array()[0].msg);
+        }
+    }
+
+    static async newForm(req: Request, res: Response): Promise<void> {
+        const user = req.session.user;
+        const languages = await prisma.language.findMany();
+        res.render('snippets/snippets_form', {languages, title: 'Nouveau snippet', section: 'Snippets', user});
+    }
+
+    static async newSnippet(req: Request, res: Response): Promise<void> {
+        const result = validationResult(req);
+        if (result.isEmpty()) {
+            const {title, code, description, languages} = req.body;
+            const user = req.session.user;
+            const languagePrisma = await prisma.language.findUnique({where: {id: parseInt(languages)}}).then((language) => {
+                return language
+            });
+
+            const userPrisma = await prisma.user.findUnique({where: {id: parseInt(user.id)}}).then((user) => {
+                return user
+            });
+            if (languagePrisma !== null && userPrisma !== null) {
+                await prisma.snippet.create({
+                    data: {
+                        title: title,
+                        code: code.replace(/(\r\n)+$/g, '').replace(/(\r\n){2,}/g, '\r\n'),
+                        description: description.replace(/(\r\n)+$/g, '').replace(/(\r\n){2,}/g, '\r\n'),
+                        creationDate: new Date(),
+                        languageId: languagePrisma.id,
+                        userId: userPrisma.id
+                    }
+                })
+            }
+            res.redirect('/');
+
         } else {
             throw new Error(result.array()[0].msg);
         }
--- src/snippets/snippets.routes.ts
+++ src/snippets/snippets.routes.ts
@@ -1,10 +1,30 @@
 import express from 'express';
 import {SnippetsController} from "./snippets.controller";
 import expressAsyncHandler from "express-async-handler";
-import {query} from "express-validator";
+import {body, query} from "express-validator";
 import {languageValidator} from "../languages/languages.middlewares";
+import {prisma} from "../services/prima";
+import {isConnected} from "../auth/auth.middleware";
 const router = express.Router();
 
 router.get('/', query('lang').optional().notEmpty().isInt().custom(async value => {await languageValidator(value)}),expressAsyncHandler(SnippetsController.list));
+router.get('/new',isConnected, expressAsyncHandler(SnippetsController.newForm));
+router.post('/new',
+    isConnected,
+    express.urlencoded({ extended: true }),
+        body('title').notEmpty().isString().isLength({ min: 5, max: 50 }).withMessage('Le titre doit contenir entre 5 et 50 caractères'),
+        body('code').notEmpty().isLength({ min: 1, max: 1000 }).withMessage('Le code doit contenir entre 1 et 1000 caractères'),
+        body('description').isString().isLength({ min: 0, max: 1000 }).withMessage('La description doit contenir au maximum 1000 caractères '),
+    async (req, res, next) => {
+        try {
+            await prisma.language.findUnique({ where: { id: parseInt(req.body.languages) }});
+            next();
+        } catch (error) {
+                next(error);
+            }
+        },
+        expressAsyncHandler(SnippetsController.newSnippet)
+    );
+
 
 export default router;
